a27782750a58a28670cb0d73b17a2ec9bc6e9517
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-21 20:38:09 +0100

    created the source code for swapping in terms of sorting for our algorithm

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap - used for swapping two element values together
+ * @xp: pointer to an array
+ * @yp: pointer to an array
+ * Return: nothing as it just swaps elements from the memory address
+ */
+void swap(int *xp, int *yp)
+{
+    int temp;
+    temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
