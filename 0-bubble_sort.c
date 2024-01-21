b23dd5395435f0a5f1c980e0bb646240c69aa13a
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-21 20:39:07 +0100

    modified the source code, removing the swap function to its own func

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,20 +3,6 @@
 #include "sort.h"
 
 /**
- * swap - used for swapping two element values together
- * @xp: pointer to an array
- * @yp: pointer to an array
- * Return: nothing as it just swaps elements from the memory address
- */
-void swap(int *xp, int *yp)
-{
-    int temp;
-    temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-/**
  * bubble_sort - used for swap
  *
  */
