20b56b9e582dc9a7abdde602f66927e444729cc9
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-21 21:33:00 +0100

    added the sorting function inside of the selection sort function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * swap - used for swapping two element values together
+ * @xp: pointer to an array
+ * @yp: pointer to an array
+ * Return: nothing as it just swaps elements from the memory address
+ */
+void swap(int *xp, int *yp)
+{
+    int temp;
+
+    temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+/**
+ * selection_sort - used for swap
+ * @array: Array of unsorted values
+ * @size: size of unsorted array
+ * Return: Nothing
+ */
 void selection_sort(int *array, size_t size)
 {
     size_t i, j, temp;
