d6ecbf5ea1d0b9e615bec4fe5edb721d517dd9e3
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-21 13:23:48 +0100

    modified the bubble sort algorithm to have a true or false value to check if two elements were swapped

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,21 +23,34 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
+    int swapped = 0;
 
     /*Checks for the size of the array if its less than 2
      * then swap wouldn't occur at all
      */
     if (size < 2)
-        exit(0);
+        exit(1);
     /* For Traversing across the array*/
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size - 1; i++)
     {
         /* For comparing values at each elements*/
         for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
+            {
                 swap(&array[j], &array[j + 1]);
-            print_array(array, size);
+                swapped = 1;
+                print_array(array, size);
+            }
         }
+        if (swapped == 0)
+            break;
     }
 }
+
+/**
+ * if it gets to the end
+ * check if the current element is greater than the last element
+ * if it is, then swap size - 1 else break
+ *
+ */
