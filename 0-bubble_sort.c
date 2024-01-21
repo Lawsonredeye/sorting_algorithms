9fcb7f75c1cf998a31dc20685b6d5cdda5d4589e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-21 21:20:57 +0100

    tweaked it lightly

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,30 +8,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int swapped = 0;
+	size_t i, j;
+	int swapped = 0;
 
-    /*Checks for the size of the array if its less than 2
-     * then swap wouldn't occur at all
-     */
-    if (size < 2)
-        exit(1);
-    /* For Traversing across the array*/
-    for (i = 0; i < size - 1; i++)
-    {
-        /* For comparing values at each elements*/
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-        if (swapped == 0)
-            break;
-    }
+	/*Checks for the size of the array if its less than 2
+	 * then swap wouldn't occur at all
+	 */
+	if (size < 2)
+		exit(1);
+	/* For Traversing across the array*/
+	for (i = 0; i < size - 1; i++)
+	{
+		/* For comparing values at each elements*/
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
 
 /**
