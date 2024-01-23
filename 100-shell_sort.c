1e1f88ff284c402addf25c9b09d1ba8add04d791
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-24 00:19:52 +0100

    shell_sort implementation using knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * shell_sort - using this method of insertion to sort an
+ * unorded array
+ * @array: unsorted/ almost sorted array
+ * @size: size of the array
+ * Return: nothing since its a void function
+ */
+void shell_sort(int *array, size_t size)
+{
+    size_t i, j, gap = 1;
+    int temp, swapped;
+
+    if (size < 2)
+        return;
+    while (gap < size)
+        gap = gap * 3 + 1;
+    gap /= 3;
+    for (; gap > 0; gap /= 3)
+    {
+        swapped = 0;
+        for (i = gap; i < size; i++)
+        {
+            temp = array[i];
+            for (j = i; (j >= gap) && (array[j - gap] > temp); j -= gap)
+            {
+                array[j] = array[j - gap];
+                swapped = 1;
+            }
+            array[j] = temp;
+        }
+        print_array(array, size);
+        if (swapped == 0)
+            break;
+    }
+}
