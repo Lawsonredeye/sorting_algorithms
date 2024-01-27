3332199281d6b194cfd95452f2543ac8e47862f9
lawsonredeye <omoregbeeolawson@gmail.com>
2024-01-27 20:37:53 +0100

    fixed indentation

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,29 +10,29 @@
  */
 void counting_sort(int *array, size_t size)
 {
-    size_t i;
-    int j, max = array[0], *count = 0, *sorted = 0, index;
+	size_t i;
+	int j, max = array[0], *count = 0, *sorted = 0, index;
 
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-    max += 1;
-    index = 0;
-    count = malloc((max + 1) * sizeof(int));
-    sorted = malloc(size * sizeof(int));
-    for (i = 0; i < size; i++)
-        count[array[i]]++;
-    for (j = 0; j < max; j++)
-    {
-        for (j = 0; j < count[i]; j++)
-        {
-            sorted[index] = i;
-            index++;
-        }
-    }
-    print_array(sorted, size);
-    free(sorted);
-    free(count);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	max += 1;
+	index = 0;
+	count = malloc((max + 1) * sizeof(int));
+	sorted = malloc(size * sizeof(int));
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	for (j = 0; j < max; j++)
+	{
+		for (j = 0; j < count[i]; j++)
+		{
+			sorted[index] = i;
+			index++;
+		}
+	}
+	print_array(sorted, size);
+	free(sorted);
+	free(count);
 }
